d35680bd523db5a790c97007a5a33bc6e23cb64b
danielcstock <danielcstock12@gmail.com>
2018-09-04 00:09:11 -0300

    Criacao da classe de Excecao.

--- ProjetoRussia2018.java
+++ ProjetoRussia2018.java
@@ -3,8 +3,8 @@ public class ProjetoRussia2018 {
     public static void main(String[] args) {
         LinkedList<Time> t = new LinkedList();
         Time a = new Time(), b = new Time();
-        //a.setProbabilidades(6.5, 4.3);
-        //b.setProbabilidades(4.9, 5.3);
+        a.setProbabilidades(6.5, 4.3);
+        b.setProbabilidades(4.9, 5.3);
         t.add(a);
         t.add(b);
         Partida p = new Partida(t);
